628ac1e221324c6167f824fa58fb80d842d680d2
piyush ujgaokar <piyushujgaokar544@gmail.com>
2025-11-13 21:42:11 +0530

    axios created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import axios from 'axios'
 import React from 'react'
 
 const Home = () => {
+
+
   return (
-    <div>Home</div>
+    <div>
+        <button>Get Products</button>
+    </div>
   )
 }
 
